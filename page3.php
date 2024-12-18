2960a8173c740db4585dc98edf280874191cf458
Andrey Semenov <semenov.orc@gmail.com>
2024-12-18 19:19:12 +0300

    add read count page3 hit

--- page3.php
+++ page3.php
@@ -6,6 +6,10 @@ declare(strict_types=1);
 header("Content-Type: text/html; charset=utf-8");
 header("Cache-Control: no-cache");
 
+// session_start();
+session_start();
+$_SESSION['countPage3'] = isset($_SESSION['countPage3']) ? ++$_SESSION['countPage3'] : 0;
+
 // содержимое
 require "getHTML.php";
 echo getHTML("Страница 3", basename(__FILE__));
--- page4.php
+++ page4.php
@@ -6,6 +6,14 @@ declare(strict_types=1);
 header("Content-Type: text/html; charset=utf-8");
 header("Cache-Control: no-cache");
 
+// session_start();
+session_start();
+
 // содержимое
 require "getHTML.php";
 echo getHTML("Страница 4", basename(__FILE__));
+echo <<<CNT
+  <div style='text-align: center; margin-top: 50px; width: 800px; margin-left: auto; margin-right: auto;'>
+    <p>Количество переходов на страницу 3: <span style='color: red; font-weight: bold; font-size: 24px'>{$_SESSION['countPage3']}</span></p>
+  </div>
+CNT;
