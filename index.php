6aa36b386a4d408ab12567dfef7c400bbecaeb72
Andrey Semenov <semenov.orc@gmail.com>
2024-12-18 14:30:45 +0300

    add header to index.php

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+header("Content-Type: text/html; charset=utf-8");
+header("Cache-Control: no-cache");
+
 echo "<div style='text-align: center; margin-top: 50px; width: 800px; margin-left: auto; margin-right: auto;'>";
 echo "<ul style='list-style-type: none; margin: 0; padding: 0; font-size: 24px; font-weight: bold; display: flex; justify-content: space-between;'>";
 echo "<li><a href='task1.php'>Страница 1</a></li>";
