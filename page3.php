433a20c772b2a736dfb953e0d03e9a077dcc8419
Andrey Semenov <semenov.orc@gmail.com>
2024-12-18 19:37:53 +0300

    add btn reload page3

--- page3.php
+++ page3.php
@@ -2,14 +2,25 @@
 
 declare(strict_types=1);
 
+session_start();
+$_SESSION['countPage3'] = isset($_SESSION['countPage3']) ? ++$_SESSION['countPage3'] : 0;
+
+if (isset($_SESSION['countPage3']) && $_SESSION['countPage3'] % 3 === 0) {
+  header("Location: page4.php");
+  exit;
+}
+
 // заголовки
 header("Content-Type: text/html; charset=utf-8");
 header("Cache-Control: no-cache");
 
-// session_start();
-session_start();
-$_SESSION['countPage3'] = isset($_SESSION['countPage3']) ? ++$_SESSION['countPage3'] : 0;
 
 // содержимое
 require "getHTML.php";
 echo getHTML("Страница 3", basename(__FILE__));
+
+echo <<<BTN
+  <div style='text-align: center; margin-top: 50px; width: 800px; margin-left: auto; margin-right: auto;'>
+    <button style='font-weight: bold; font-size: 24px; padding: 10px; border-radius: 10px; cursor: pointer;' onclick='window.location.reload()'>Перегрузить страницу</button>
+  </div>
+BTN;
--- page4.php
+++ page4.php
@@ -2,18 +2,17 @@
 
 declare(strict_types=1);
 
+session_start();
+
 // заголовки
 header("Content-Type: text/html; charset=utf-8");
 header("Cache-Control: no-cache");
-
-// session_start();
-session_start();
 
 // содержимое
 require "getHTML.php";
 echo getHTML("Страница 4", basename(__FILE__));
 echo <<<CNT
   <div style='text-align: center; margin-top: 50px; width: 800px; margin-left: auto; margin-right: auto;'>
-    <p>Количество переходов на страницу 3: <span style='color: red; font-weight: bold; font-size: 24px'>{$_SESSION['countPage3']}</span></p>
+    <p style='font-weight: bold; font-size: 24px'>Количество переходов на страницу 3: <span style='color: red; font-weight: bold; font-size: 36px'>{$_SESSION['countPage3']}</span></p>
   </div>
 CNT;
