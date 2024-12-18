02f9673caa716e58c318ee85e19db01520c6e5e4
Andrey Semenov <semenov.orc@gmail.com>
2024-12-18 14:22:12 +0300

    add markup to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+echo "<div style='text-align: center; margin-top: 50px; width: 800px; margin-left: auto; margin-right: auto;'>";
+echo "<ul style='list-style-type: none; margin: 0; padding: 0; font-size: 24px; font-weight: bold; display: flex; justify-content: space-between;'>";
+echo "<li><a href='task1.php'>Страница 1</a></li>";
+echo "<li><a href='task2.php'>Страница 2</a></li>";
+echo "<li><a href='task2.php'>Страница 3</a></li>";
+echo "<li><a href='task2.php'>Страница 4</a></li>";
+echo "</ul>";
+echo "</div>";
